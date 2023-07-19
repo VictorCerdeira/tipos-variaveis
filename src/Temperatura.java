257abf15f7f1f138b8057110ace68f947f751d2a
VictorCerdeira <victorcerd3@gmail.com>
2023-07-19 12:19:52 -0300

    exercicio temperatura

---
+++ src/Temperatura.java
@@ -0,0 +1,16 @@
+public class Temperatura {
+   
+
+        public static void main(String[] args) throws Exception {
+            double C, K, F;
+            C = 3.43;
+            F = C * 1.8 + 32;
+            K = C + 273.15;
+    
+            System.out.println("A temperatura em Fahrenheit é:" + F);
+            System.out.println("A temperatura em Kelvin é: " + K);
+
+
+            
+    }
+}
